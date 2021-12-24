81ba5fbb806a27fc1bb5351735f700cda1d19074
feiben <feiben.dev@gmail.com>
2021-12-24 11:54:27 +0800

    fix: reactivity track bug

--- src/reactivity/__test__/reactive.spec.js
+++ src/reactivity/__test__/reactive.spec.js
@@ -21,6 +21,25 @@ describe('测试响应式', () => {
     expect(val).toBe(10)
   })
 
+  test('一个reactive对象的属性在多个effect中使用', () => {
+    const ret = reactive({ num: 0 })
+    let val, val2
+    effect(() => {
+      val = ret.num
+    })
+    effect(() => {
+      val2 = ret.num
+    })
+    expect(val).toBe(0)
+    expect(val2).toBe(0)
+    ret.num++
+    expect(val).toBe(1)
+    expect(val2).toBe(1)
+    ret.num = 10
+    expect(val).toBe(10)
+    expect(val2).toBe(10)
+  })
+
   test('shalldowReactive基本使用', () => {
     const ret = shallowReactive({ num: 0 })
     let val
